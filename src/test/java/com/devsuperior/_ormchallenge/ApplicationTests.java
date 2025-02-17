875cb55dd89e605d4d11fabdb0db0d7bab1f4b78
Keizo Kobayashi <keizo.kobayashi@outlook.com.br>
2025-02-17 01:36:11 -0300

    Project Created

---
+++ src/test/java/com/devsuperior/_ormchallenge/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior._ormchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
